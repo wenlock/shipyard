112c0ea1ccac867981b998b096647e1b8a9ec902
Ricardo Quintana <ricardo.quintana@hpe.com>
2016-05-12 15:27:24 -0400

    Added HealthChecker background process for Registered providers.

--- model/provider.go
+++ model/provider.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"go/types"
+	"time"
 )
 
 type Provider struct {
@@ -11,6 +12,7 @@ type Provider struct {
 	Config            types.Object   `json:"config" gorethink:"config"`
 	Url               string         `json:"url" gorethink:"url"`
 	ProviderJobs      []*ProviderJob `json:"providerJobs" gorethink:"providerJobs"`
+	Health            Health         `json:"health" gorethink:"health"`
 }
 
 func (p *Provider) NewProvider(name string, availableJobTypes types.Array, config types.Object, url string, providerJobs []*ProviderJob) *Provider {
@@ -24,9 +26,16 @@ func (p *Provider) NewProvider(name string, availableJobTypes types.Array, confi
 	}
 }
 
+type Health struct {
+	Url        string    `json:"url" gorethink:"url"`
+	Status     string    `json:"status" gorethink:"status"`
+	LastUpdate time.Time `json:"lastUpdate" gorethink:"lastUpdate"`
+}
+
 type ProviderJob struct {
-	ID   string `json:"id,omitempty" gorethink:"id,omitempty"`
+	//ID   string `json:"id,omitempty" gorethink:"id,omitempty"`
 	Name string `json:"name" gorethink:"name"`
+	Url  string `json:"url" gorethink:"url"`
 }
 
 func (p *ProviderJob) NewProviderJob(name string) *ProviderJob {
