8a2339906c036fe727ac2c89454b3909f461e341
Jonathan Rosado <rosadoft@gmail.com>
2016-06-09 12:59:01 -0400

    - WIP. Fixed tests.

--- controller/api/projects_test.go
+++ controller/api/projects_test.go
@@ -52,7 +52,7 @@ func init() {
 
 	// Default docker endpoint
 	if dockerEndpoint == "" {
-		dockerEndpoint = "tcp://127.0.0.1:2375"
+		dockerEndpoint = "tcp://proxy:2375"
 	}
 
 	rethinkDbEndpoint := os.Getenv("SHIPYARD_RETHINKDB_URI")
